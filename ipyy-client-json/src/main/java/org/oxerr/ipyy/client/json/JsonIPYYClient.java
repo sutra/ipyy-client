699b3ad85ce7a82bc5bae9d22a1c1336ab3d1f91
Sutra Zhou <zhoushuqun@gmail.com>
2019-07-17 18:07:58 +0800

    Rename JacksonIPYYClient to JsonIPYYClient.

--- ipyy-client-json/src/main/java/org/oxerr/ipyy/client/json/JsonIPYYClient.java
+++ ipyy-client-json/src/main/java/org/oxerr/ipyy/client/json/JsonIPYYClient.java
@@ -2,9 +2,9 @@ package org.oxerr.ipyy.client.json;
 
 import org.oxerr.ipyy.client.core.IPYYClient;
 
-public class JacksonIPYYClient extends IPYYClient {
+public class JsonIPYYClient extends IPYYClient {
 
-	public JacksonIPYYClient(String account, String password) {
+	public JsonIPYYClient(String account, String password) {
 		super("https://dx.ipyy.net/smsJson.aspx", new ReturnSMSJacksonUnmarshaller(), account, password);
 	}
 
